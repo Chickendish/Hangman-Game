13ff275ca1ee4e543aa826f4b4e198a21ee9ddb4
Bill Donovan <williamcdonovan@gmail.com>
2017-05-18 15:53:25 -0600

    properly linked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,24 @@
 
 window.onload = function () {
 
+var wins = 0;
+var losses = 0;
+var lives = 3;
+
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+
 console.log(alphabet[3]);
 
 }
+
+//Player will need to press any key to begin
+
+//random generator will pick a record in an array
+
+//Player will pick 5 letters and have three chances
+
+
+

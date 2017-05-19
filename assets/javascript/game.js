970236f3cd5371b8c987776257561633d310ee7d
Bill Donovan <williamcdonovan@gmail.com>
2017-05-19 12:45:57 -0600

    added letter box options

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,17 +5,29 @@ window.onload = function () {
 var wins = 0;
 var losses = 0;
 var lives = 3;
+var Wrestlers = ['ric flair','hulk hogan','stone cold steve austin','andre the giant','roddy piper'];
+var Painter = ['michelangelo','titian','rafael','botticelli','giorgione','hieronymous bosch']
 var cities = ['new york','paris','london','osaka','toyko','mumbai','los angeles','mexico city','jakarta','lagos'];
 console.log(cities[4]);
 
 $(".Wrestlers").on("click",function() {
 	$(".Wrestlers").fadeOut(3000);
-	console.log("in the function");
+	$
+	//console.log("in the function");
 });
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+for (var i = 0;i < alphabet.length; i++) {
+	var letterBtn = $("<button>");
+	console.log("here this far");
+	letterBtn.addClass("letter-button letter letter-button-color");
+	letterBtn.attr("data-letter", alphabet[i]);
+	letterBtn.text(alphabet[i]);
+	$("#letterBox").append(letterBtn);
+};
 
 
 console.log(alphabet[3]);

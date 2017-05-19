a95d50214d6705b84fdaeb3a92bed564eeeaac54
Bill Donovan <williamcdonovan@gmail.com>
2017-05-19 10:52:54 -0600

    used jQuery to grab the Wrestlers button and fadeOut

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,13 @@ window.onload = function () {
 var wins = 0;
 var losses = 0;
 var lives = 3;
-var cities = ['new york','paris','london','osaka','toyko','mumbai'];
+var cities = ['new york','paris','london','osaka','toyko','mumbai','los angeles','mexico city','jakarta','lagos'];
 console.log(cities[4]);
+
+$(".Wrestlers").on("click",function() {
+	$(".Wrestlers").fadeOut(3000);
+	console.log("in the function");
+});
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',

fc8c13a0eaee029c9ebf06342d6adbd0de7260ac
Bill Donovan <williamcdonovan@gmail.com>
2017-05-21 13:50:45 -0600

    things are messed up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,11 +7,13 @@ window.onload = function () {
 var wins = 0;
 var losses = 0;
 var lives = 3;
+var topic = "";
 
 //topic arrays
 var Wrestlers = ['ric flair','hulk hogan','stone cold steve austin','andre the giant','roddy piper'];
 var Painters = ['michelangelo','titian','rafael','botticelli','giorgione','hieronymous bosch'];
 var Cities = ['new york','paris','london','osaka','toyko','mumbai','los angeles','mexico city','jakarta','lagos'];
+var answerArray = [];
 
 //game pieces
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
@@ -20,33 +22,36 @@ var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 
 // user picks a topic
 
+
 $(".Wrestlers").on("click",function(topic) {
-	var secretAnswer = Wrestlers[Math.floor(Math.random()*Wrestlers.length)];
-	console.log(secretAnswer);
-	$(".Painter").fadeOut(3000);
-	$(".Cities").fadeOut(3000);
+	var topic = Wrestlers[Math.floor(Math.random()*Wrestlers.length)];
+	$(".Painters").hide();
+	$(".Cities").hide();
+	$("#hiddenAnswer").text(topic);
 
 });
 
+
 $(".Painters").on("click",function(topic) {
-	var secretAnswer = Painters[Math.floor(Math.random()*Painters.length)];
-	console.log(secretAnswer);
-	$(".Wrestlers").fadeOut(3000);
-	$(".Cities").fadeOut(3000);
+	var topic = Painters[Math.floor(Math.random()*Painters.length)];
+	console.log(topic);
+	$(".Wrestlers").hide();
+	$(".Cities").hide();
+	$("#hiddenAnswer").text(topic);
 	
 });
 
 $(".Cities").on("click",function(topic) {
-	var secretAnswer = Cities[Math.floor(Math.random()*Cities.length)];
-	console.log(secretAnswer);
-	$(".Painters").fadeOut(2000);
-	$(".Wrestlers").fadeOut(2000);
-	
+	var topic = Cities[Math.floor(Math.random()*Cities.length)];
+	console.log(topic);
+	$(".Painters").hide();
+	$(".Wrestlers").hide();
+	$("#hiddenAnswer").text(topic);
 });
+
 
 for (var i = 0;i < alphabet.length; i++) {
 	var letterBtn = $("<button>");
-	console.log("here this far");
 	letterBtn.addClass("letter-button letter letter-button-color");
 	letterBtn.attr("data-letter", alphabet[i]);
 	letterBtn.text(alphabet[i]);

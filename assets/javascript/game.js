f1c09b259daf04f81592b9f9ad79524cd2055a2f
Bill Donovan <williamcdonovan@gmail.com>
2017-05-24 11:38:42 -0600

    topic button works, populate guess input with blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,20 @@
 window.onload = function () {
 
-//game tally
-
+//tally panel
 var wins = 0;
 var losses = 0;
-var lives = 3;
+var guessesRemaining = 6;
 
-//topic arrays
-var Wrestlers = ['ric flair','hulk hogan','stone cold steve austin','andre the giant','roddy piper'];
-var Painters = ['michelangelo','titian','rafael','botticelli','giorgione','hieronymous bosch'];
-var Cities = ['new york','paris','london','osaka','toyko','mumbai','los angeles','mexico city','jakarta','lagos'];
-
-// checks if a topic has been selected
-var topic;
-// this should live outside the click functions so we can reference it from anywhere (remember the scope discussion)
-var secretAnswer
-// hold the blanks
+//guess panel
+var topic = "";
+var secretAnswer = [];
 var blanks = [];
 
-var blanksArray = [];
+//alphabet for letter boxes
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-// create the array of blanks
+var wrestlers = ['rowdy roddy piper','jimmy supafly snuka','hulk hogan','the undertaker','andre the giant','randy savage','stone cold steve austin','ric flair'];
+
 function makeBlanks() {
 	for (i=0; i<secretAnswer.length; i++) {
 		blanks[i] = "_";
@@ -28,96 +22,11 @@ function makeBlanks() {
 	$("#guessInput").text(blanks.join(" "));
 }
 
-//game pieces
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-// user picks a topic
-// These could be streamlined, but works fine as three different functions
-$(".Wrestlers").on("click",function() {
-	// only select a topic if one hasn't been picked already
-	if (topic == undefined) {
-		secretAnswer = Wrestlers[Math.floor(Math.random()*Wrestlers.length)];
-		console.log(secretAnswer);
-		$(".Painter").fadeOut(3000);
-		$(".Cities").fadeOut(3000);
-		// set the topic to the button's text
-		topic = $(this).text();
-		makeBlanks();
-	}
-
-});
-
-$(".Painters").on("click",function() {
-	if (topic == undefined) {
-		secretAnswer = Painters[Math.floor(Math.random()*Painters.length)];
-		console.log(secretAnswer);
-		$(".Wrestlers").fadeOut(3000);
-		$(".Cities").fadeOut(3000);
-		topic = $(this).text();
-		makeBlanks();
-	}
-});
-
-$(".Cities").on("click",function() {
-	if (topic == undefined) {
-		secretAnswer = Cities[Math.floor(Math.random()*Cities.length)];
-		console.log(secretAnswer);
-		$(".Painters").fadeOut(3000);
-		$(".Wrestlers").fadeOut(3000);
-		topic = $(this).text();
-		makeBlanks();
-	}
-	
-});
-
-for (var i = 0;i < alphabet.length; i++) {
-	var letterBtn = $("<button>");
-	letterBtn.addClass("letter-button letter letter-button-color");
-	letterBtn.attr("data-letter", alphabet[i]);
-	letterBtn.text(alphabet[i]);
-	$("#letterBox").append(letterBtn);
-};
-
-$(".letter").on("click",function() {
-	// if no topic picked, alert the user
-	if (topic == undefined) {
-		alert("Select a topic!");
-	} else {
-		// if a topic has been picked, start checking the letters
-		var letterGuess = $(this).text();
-		console.log(letterGuess);
-
-		if (secretAnswer.includes(letterGuess)) {
-			console.log("That letter is in the word");
-			for (i = 0; i < secretAnswer.length; i++) {
-				secretAnswer.split("") = blanksArray[];
-
-				//turn secretAnswer into an array (new variable)
-				//make a another new array (blanksArray) []
-				//for loops - blanksArray.push("_")
-				// 1 to 1 correspondence between blanksArray and secretAnswer
-
-			}
-		}
-
-		else {
-			losses++;
-		}
-		//if $(letterGuess == secretAnswer):
-
-		//1. compare letterGuess to secretAnswer [hint: use .includes()]
-
-		//2. if correct, change the same index of blanks as the correct index of secretAnswer
-
-		//3. if wrong, decrement lives
-
-		//4. if word is guess, win++
-
-		//5. if no lives, losses++
-
-	}
+$("#topic").on("click", function(){
+	secretAnswer = wrestlers[Math.floor(Math.random()*wrestlers.length)];
+	console.log(secretAnswer);
+	topic = $(this).text();
+	makeBlanks();
 });
 
 }
